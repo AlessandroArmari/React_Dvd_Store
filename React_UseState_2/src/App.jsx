23a1f83cb16881374556ee8498508c450b1f84a1
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-01-15 00:16:22 +0100

    ++,-- to be fixed

--- React_UseState_2/src/App.jsx
+++ React_UseState_2/src/App.jsx
@@ -10,39 +10,34 @@ var list = [
 function App() {
   //Declaring the "hook"
   //Remember to import { useState }
-  const [addToBasket, setAddToBasket] = useState(0); //--->default value is "0"
-
+  const [addToBasket, setAddToBasket] = useState(); //--->default value is "0"
+  const [showBasket, setShowBasket] = useState(false); //--->to set the basket visibility
   return (
     <div>
-      <input placeholder="Basket" value={addToBasket} />
+      {/* here I'm saying:
+      because of the  {} --->  this input is a js item
+      && is similar to an if sentence:
+      "if showBasket AND input_item (which is ALWAYS true here) ARE BOTH TRUE"--->
+      --->&& returns the last item of the list
+      --->so it will return input item 
+      So, ONLY IF showBasket is true, the input will be shown!*/}
+      {showBasket && <input placeholder="Basket" value={addToBasket} />}
       <div>
         {list.map((elem, index) => {
-          //ADDING FUNCTIONS TO ASSIGN TO THE onClik events
-          /*1 function*/
-          //this function does two operations!
+          //CREATE FUNCTION IN ORDER TO ASSIGN IT LATER TO THE onClik events
 
-          //FIRST:
-          var addButtonFunction = (addToBasket) => {
-            setAddToBasket(addToBasket.target.value++);
-            //SECOND:
-
-            console.log(
-              "You've added: " +
-                elem.dvd_name +
-                " (item_number: " +
-                index +
-                "), price: " +
-                elem.dvd_price +
-                ", to the basket"
-            );
-          };
-          /*2 function*/
           var removeButtonFunction = (addToBasket) => {
+            //FIRST part:
+
             setAddToBasket(addToBasket.target.value--);
+            //SECOND part:
 
             console.log(
               "You've removed: " + elem.dvd_name + " from the basket"
             );
+            //THIRD part:
+
+            setShowBasket(true);
           };
           //This functions must be located HERE: Where they can find the variables they need to work
           //Not inside the return down here, not outside (they won't find the variables")
@@ -52,10 +47,32 @@ function App() {
               key={index}
               dvd_name={elem.dvd_name}
               dvd_price={elem.dvd_price}
-              addButton={addButtonFunction}
+              addButton={(addToBasket) => {
+                //+++FOR THE addButton I'm typing the function here+++
+                //+++GO DOWN TO removeButton for see something else!
+                //FIRST part:
+
+                var increasingValue = addToBasket.target.value++;
+                setAddToBasket(increasingValue);
+                //SECOND part:
+
+                console.log(
+                  "You've added: " +
+                    elem.dvd_name +
+                    " (item_number: " +
+                    index +
+                    "), price: " +
+                    elem.dvd_price +
+                    ", to the basket"
+                );
+                //THIRD part (change basket visibility whit showBasket)
+
+                setShowBasket(true); //--->changing the default value (false) of ShowBasket to "true"
+              }} //--->once we cliccked once, it will be "true, NOW GO TO INPUT, above"
               removeButton={removeButtonFunction}
-              // Assigning to the <Card/> props. "addButton" and "removeButton"
-              // the functions I declared above
+              // Here I do not type the function directly!
+              // Assigning to the <Card/> props."removeButton"
+              // the functions I declared above.
             />
           );
         })}
